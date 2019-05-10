7a8b8d79fbac8836932b0eb6b893658d950b5070
FlorianW <florian.weischer@gmail.com>
2019-05-10 18:53:55 +0200

    no message

--- src/obj/bhkBoxShape.cpp
+++ src/obj/bhkBoxShape.cpp
@@ -125,7 +125,7 @@ Vector3 bhkBoxShape::GetDimensions() const {
 
 void bhkBoxShape::SetDimensions(const Vector3 &value) {
 	dimensions = value;
-	minimumSize = min( min(value.x, value.y), value.z );
+	minimumSize = fmin( fmin(value.x, value.y), value.z );
 }
 
 void bhkBoxShape::CalcMassProperties( float density, bool solid, float &mass, float &volume, Vector3 &center, InertiaMatrix& inertia )
